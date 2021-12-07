8c4247aa5a42353d33240e2fc6bb2414011b8738
Konrad Sochacki <sochacki.konrad@gmail.com>
2021-12-07 13:21:21 +0100

    Update install.sh msg

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ mkdir ~/.hit_install
 cp -r * ~/.hit_install
 chmod u+x ~/.hit_install hit
 # Make globally available
-PATH=$PATH:~/.hit_install
 echo '# Hit Semantive' >> ~/.bashrc
 echo 'export PATH="${PATH}:$HOME/.hit_install"' >> ~/.bashrc
 
 echo Hit has been installed to you HOME dir and added to path
-echo You can try it by typing 'hit' in console
+echo Create new console or source ~/.bashrc to use it
+echo You can try it by typing 'hit'
 
