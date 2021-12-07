c7b8aa799d7ad33475e0ebe1f491d5fb7ddee22f
Konrad Sochacki <sochacki.konrad@gmail.com>
2021-12-07 13:22:53 +0100

    Implement calling hit processing functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hi Semantive")
+import sys
+
+from hit_processing.processing_func import hit_init, hit_status
+
+if len(sys.argv) == 1:
+    print("Hit Semantive version 0.01")
+    sys.exit(0)
+
+cmd = sys.argv[1]
+if cmd == "init":
+    hit_init()
+elif cmd == "status":
+    hit_status()
+else:
+    print(f"Unrecognized command: {cmd}")
