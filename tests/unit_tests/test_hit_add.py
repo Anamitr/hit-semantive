fd85e3587c679d5eaf2482d35c896c025bc01804
Konrad Sochacki <sochacki.konrad@gmail.com>
2021-12-09 12:15:09 +0100

    Add test_hit_add_not_changed_file

--- tests/unit_tests/test_hit_add.py
+++ tests/unit_tests/test_hit_add.py
@@ -1,8 +1,10 @@
 import subprocess
 
 from processing.hit_add import hit_add
+from processing.hit_commit import hit_commit
 from processing.hit_init import hit_init
 from processing.hit_processing import read_hit_content
+from processing.hit_status import hit_status
 
 
 def test_hit_add_invalid_file(test_dir, capsys):
@@ -62,3 +64,21 @@ def test_hit_add_multiple(test_dir):
     hit_content = read_hit_content(test_dir)
 
     assert hit_content["staged"] == ["file1", "file2", "file3"]
+
+
+def test_hit_add_not_changed_file(test_dir, capsys):
+    """ GIVEN hit repository with commited files
+        WHEN hit_add(not_changed_file) is called
+        THEN hit_status should show no staged file
+    """
+    hit_init()
+    subprocess.run("touch file1; touch file2", shell=True)
+    hit_add("file1")
+    hit_commit()
+    capsys.readouterr().out = ""
+
+    hit_add("file1")
+
+    hit_status()
+    out = capsys.readouterr().out
+    assert "> file1 (staged file)" not in out
